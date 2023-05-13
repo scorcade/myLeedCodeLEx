2d26337eb480afcff477336c9f8d6db79e990501
scorcade <scorcade@outlook.com>
2023-05-13 14:49:21 +0800

    Add files via upload

---
+++ 2441  Largest Positive Integer That Exists With Its Negative.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+Return the positive integer k. If there is no such integer, return -1.
+
+Input: nums = [-1, 2, -3, 3]
+Output: 3
+Explanation: 3 is the only valid k we can find in the array.*/
+
+
+public class Solution
+{
+    public int FindMaxK(int[] nums)
+    {
+        int k = -1;
+        foreach (int x in nums)
+        {
+            foreach (int y in nums)
+            {
+                if (-x == y) k = Math.Max(y, k);
+            }
+        }
+        return k;
+    }
+}
