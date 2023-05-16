e6ad649ba269c5f27494541aabef4765f6932331
scorcade <scorcade@outlook.com>
2023-05-16 23:49:57 +0800

    Update 13  Roman to Integer.cs

--- 13  Roman to Integer.cs
+++ 13  Roman to Integer.cs
@@ -134,30 +134,26 @@ Explanation: L = 50, V = 5, III = 3.*/
 
 
 
+//#class Solution:   #5/16/2023 11:47 PM  PYTHON
+//#    SYMBOL_VALUES={
+//#        'I':1,
+//#        'V':5,
+//#        'X':10,
+//#        'L':50,
+//#        'C':100,
+//#        'D':500,
+//#        'M':1000,
+//#        }
 
+//#    def romanToInt(self, s:str)->int:
+//#        ans=0
+//#        n=len(s)
 
-#class Solution:   #5/16/2023 11:47 PM  PYTHON
-#    SYMBOL_VALUES={
-#        'I':1,
-#        'V':5,
-#        'X':10,
-#        'L':50,
-#        'C':100,
-#        'D':500,
-#        'M':1000,
-#        }
+//#        for i,ch in enumerate(s):
+//#            value=Solution.SYMBOL_VALUES[ch]
+//#            if i<n-1 and value < Solution.SYMBOL_VALUES[s[i+1]]:
+//#                ans -= value
+//#            else:
+//#                ans += value
 
-#    def romanToInt(self, s:str)->int:
-#        ans=0
-#        n=len(s)
-
-#        for i,ch in enumerate(s):
-#            value=Solution.SYMBOL_VALUES[ch]
-#            if i<n-1 and value < Solution.SYMBOL_VALUES[s[i+1]]:
-#                ans -= value
-#            else:
-#                ans += value
-
-#        return ans
-
-
+//#        return ans
