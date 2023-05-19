763d8db02eae5ed2fc5a4bea2d60bd756173ca50
scorcade <scorcade@outlook.com>
2023-05-19 22:55:02 +0800

    Create 20  Valid Parentheses.cs

---
+++ 20  Valid Parentheses.cs
@@ -0,0 +1,43 @@
+//20.Valid Parentheses
+//    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+//An input string is valid if:
+
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+//有效字符串需满足：
+
+//左括号必须用相同类型的右括号闭合。
+//左括号必须以正确的顺序闭合。
+//每个右括号都有一个对应的相同类型的左括号。
+
+//来源：力扣（LeetCode）
+//链接：https://leetcode.cn/problems/valid-parentheses
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+//Example 1:
+
+//Input: s = "()"
+//Output: true
+//Example 2:
+
+//Input: s = "()[]{}"
+//Output: true
+//Example 3:
+
+//Input: s = "(]"
+//Output: false
+
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//5/19/2023 10:54 PM
