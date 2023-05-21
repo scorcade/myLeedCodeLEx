68b06c2498597447f433ee1b383d9ef39c71cece
scorcade <scorcade@outlook.com>
2023-05-22 00:21:05 +0800

    Update and rename 26  Remove Duplicates from Sorted Array.cs to 26    Remove Duplicates from Sorted Array.cs

---
+++ 26    Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,39 @@
+//https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
+//5/21/2023 11:42 PM 
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _2441__Largest_Positive_Integer_That_Exists_With_Its_Negative
+//{
+//    internal class _26______Remove_Duplicates_from_Sorted_Array
+//    {
+//    }
+//}
+//public class Solution   5/22/2023 12:20 AM 
+//{
+//    public int RemoveDuplicates(int[] nums)
+//    {
+//        if (nums == null) return 0;
+//        int numsLen = nums.Length;
+//        if (numsLen == 1) return 1;
+
+
+//        int slowP = 1, fastP = 1;
+
+//        for (; fastP < numsLen; fastP++)
+//        {
+//            if (nums[fastP - 1] != nums[fastP])
+//            {
+//                nums[slowP] = nums[fastP];
+//                slowP++;
+//            }
+//            //fastPointer++;
+//        }
+
+//        return slowP;
+//    }
+//}
--- 26  Remove Duplicates from Sorted Array.cs
+++
@@ -1,2 +0,0 @@
-//https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
-//5/21/2023 11:42 PM 
