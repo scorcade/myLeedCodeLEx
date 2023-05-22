bc0607cd778b19a1e962f7cddd43d085a202b5e2
scorcade <scorcade@outlook.com>
2023-05-22 19:44:27 +0800

    Update 27  Remove Element.cs

--- 27  Remove Element.cs
+++ 27  Remove Element.cs
@@ -42,3 +42,49 @@
 
 //    }
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int RemoveElement(int[] nums, int val)
+//{
+//    if (nums.Length <= 1)
+//    {
+//        if (nums.Length == 0) return 0;
+//        if (nums.Length == 1 && nums[0] == val) return 0;
+//        else return 1;
+//    }
+//    int n = 0;
+//    int i = 0;
+//    int c = nums.Length;
+
+//    while (i < nums.Length )
+//    {
+//        if (nums[i] == val)
+//        {
+//            // i++;
+//            c--;
+//            //nums[n] = nums[i];
+//        }
+//        else
+//        {
+//            nums[n] = nums[i];
+//            n++;
+//            //i++;
+//        }
+//        i++;
+
+//    }
+
+//    return c;
