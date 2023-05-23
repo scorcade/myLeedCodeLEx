517825c6be069d5cfe4299b9432c88716913c9ef
scorcade <scorcade@outlook.com>
2023-05-23 10:43:23 +0800

    Update 35  Search Insert Position.cs

--- 35  Search Insert Position.cs
+++ 35  Search Insert Position.cs
@@ -1,1 +1,34 @@
 
+//35.Search Insert Position  https://leetcode.cn/problems/search-insert-position/
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _2441__Largest_Positive_Integer_That_Exists_With_Its_Negative
+//{
+//    internal class _35______Search_Insert_Position
+//    {
+//    }
+//}
+
+// 5/23/2023 10:01 AM  2023/05/23 10:42
+//public class Solution
+//{
+//    public int SearchInsert(int[] nums, int target)
+//    {
+//        int L = 0, R = nums.Length - 1;
+
+
+//        while (L <= R)
+//        {
+//            int M = (R + L) / 2;
+//            if (nums[M] == target) return M;
+//            else if (nums[M] < target) L = M + 1;
+//            else if (nums[M] > target) R = M - 1;
+//        }
+//        return L;
+//    }
+//}
