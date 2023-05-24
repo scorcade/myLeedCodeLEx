50d7475a6ddc7c0603cb0709069f7ca886f8576b
scorcade <scorcade@outlook.com>
2023-05-24 18:50:54 +0800

    Create 66  Plus One.cs

---
+++ 66  Plus One.cs
@@ -0,0 +1,15 @@
+//66. Plus One https://leetcode.cn/problems/plus-one/solution/  
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _2441__Largest_Positive_Integer_That_Exists_With_Its_Negative
+//{
+//    internal class _66______Plus_One
+//    {
+//    }
+//}
