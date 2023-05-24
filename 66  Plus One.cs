c4a915876731eb54a3da749e7f32cc55daf61349
scorcade <scorcade@outlook.com>
2023-05-24 19:08:02 +0800

    Update 66  Plus One.cs

--- 66  Plus One.cs
+++ 66  Plus One.cs
@@ -13,3 +13,29 @@
 //    {
 //    }
 //}
+
+
+//public class Solution //5/24/2023 6:57 PM    2023/05/24 19:07
+
+//{
+//    public int[] PlusOne(int[] digits)
+//    {
+//        int L = digits.Length;
+
+//        for (int i = L - 1; i >= 0; i--)
+//        {
+//            if (digits[i] != 9)
+//            {
+//                digits[i]++;
+
+//                for (int j = i + 1; j < L; j++) { digits[j] = 0; }
+//                return digits;
+//            }
+
+//        }
+
+//        int[] newDigits = new int[L + 1];
+//        newDigits[0] = 1;
+//        return newDigits;
+//    }
+//}
