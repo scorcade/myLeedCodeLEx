b670d38df07a7866b434e96d3bf68b01a353722d
scorcade <scorcade@outlook.com>
2023-05-28 18:50:34 +0800

    Create 104  Maximum Depth of Binary Tree.cs

---
+++ 104  Maximum Depth of Binary Tree.cs
@@ -0,0 +1,38 @@
+//104. Maximum Depth of Binary Tree https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _2441__Largest_Positive_Integer_That_Exists_With_Its_Negative
+//{
+//    internal class _104_____Maximum_Depth_of_Binary_Tree
+//    {
+//    }
+
+//}
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution  //5/28/2023 6:47 PM 
+
+{
+    public int MaxDepth(TreeNode root)
+    {
+
+    }
+}
