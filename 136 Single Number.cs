252e7e679e37bfccac82199a93fd28744660473a
scorcade <scorcade@outlook.com>
2023-05-29 16:46:51 +0800

    Update 136 Single Number.cs

--- 136 Single Number.cs
+++ 136 Single Number.cs
@@ -7,3 +7,26 @@
 //        return n;
 //    }
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+//public static int SingleNumber(int[] nums)  //2023/05/29 16:45
+//{
+//       HashSet<int> result = new HashSet<int>();
+
+//    for (int i = 0; i < nums.Length; i++)
+//    {
+//        if (!result.Add(nums[i])) { result.Remove(nums[i]); }
+//    }
+//    return result.First();
+
+//}
